08835f7950151d16e4231588a3ec7ad844ad531c
Moritz Juedes <mail@moritzjuedes.de>
2016-05-05 12:09:51 +0200

    Finish off part 4 of the django tutorial (next is part 5: writing tests)

--- polls/urls.py
+++ polls/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
 	# /polls/
-	url(r'^$', views.index, name='index'), 
+	url(r'^$', views.IndexView.as_view(), name='index'), 
 	# /polls/5/ # the 'name' value as called by the {% url %} template tag
-	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 	# /polls/5/results/
-	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+	url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 	# /polls/5/vote/
 	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]	
@@ -17,3 +17,5 @@ urlpatterns = [
 # url() nimmt vier arguments: (regex, view, kwags, name)
 # regex & view sind required, kwargs und name nicht
 # name vergibt einen Namen, so dass dieser von anderswo angesteuert werden kann
+
+# Hier sind generic views und functionbased views gemischt.
